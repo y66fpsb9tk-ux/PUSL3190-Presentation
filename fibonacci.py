2df58ee37fad890558c49e464faf8a3164a0afa8
y66fpsb9tk-ux <y66fpsb9tk@privaterelay.appleid.com>
2026-05-25 23:18:34 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,13 @@
-# New Python file
+def fibonacci(n):
+    a, b = 0, 1
+    result = []
+
+    for _ in range(n):
+        result.append(a)
+        a, b = b, a + b
+
+    return result
+
+
+print("First 10 Fibonacci numbers:")
+print(fibonacci(10))# New Python file
