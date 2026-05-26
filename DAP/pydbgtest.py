b27a1f8a6149cbdfb28e10ed43adcb6d48f07158
y66fpsb9tk-ux <y66fpsb9tk@privaterelay.appleid.com>
2026-05-26 09:44:59 +0530

    Update pydbgtest.py

--- DAP/pydbgtest.py
+++ DAP/pydbgtest.py
@@ -1,1 +1,11 @@
-# New Python file
+
+def divide_numbers(a, b):
+    result = a / b
+    return result
+
+
+numbers = [10, 5, 0, 2]
+
+for num in numbers:
+    output = divide_numbers(100, num)
+    print(f"100 / {num} = {output}")
